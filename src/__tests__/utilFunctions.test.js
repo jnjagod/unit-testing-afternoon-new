31e21d8e66d6b047a9e8e52ecd48696982752d0c
jnjagod <josh.jagoda@gmail.com>
2019-11-11 12:44:05 -0800

    complete assignment

---
+++ src/__tests__/utilFunctions.test.js
@@ -0,0 +1,24 @@
+import { shortenText } from '../utils/functions'
+import { wordCount, attachUserName } from '../../server/utils'
+import { shortText, longText, posts, users } from './__data__/testData'
+
+it('shortenText will not alter a string with less than 100 characters', () => {
+  expect(shortenText(shortText)).toHaveLength(29)
+})
+it('shortenText should trim string to 100 characters and add ...', () => {
+  const shortened = shortenText(longText)
+  expect(shortened).not.toHaveLength(longText.length)
+  expect(shortened.slice(-3)).toBe('...')
+})
+it('wordCount should correctly count the number of words in a sentence', () => {
+  expect(wordCount(posts)).toBe(233)
+})
+it('attachUserName should attach username to post', () => {
+  const newPosts = attachUserName(users, posts)
+  expect(newPosts[0]).toHaveProperty('displayName')
+})
+it('attachUserName should remove any post with no matching user', () => {
+  const newPosts = attachUserName(users, posts)
+  const deletedPost = posts[5]
+  expect(newPosts).not.toContainEqual(deletedPost)
+})
